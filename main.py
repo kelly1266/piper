bda0628002a3f9e295173213d58876393af8eecf
Thomas <t.kelly79285@gmail.com>
2018-12-02 12:25:40 -0500

    updated the control flow, added the ability to roll dice, also added the ability to request a description of each function

--- main.py
+++ main.py
@@ -1,66 +1,91 @@
 import discord
 import asyncio
+from discord.ext.commands import Bot
+import random
+import requests
 
 
+BOT_PREFIX='!piper '
 #Piper token
 TOKEN = 'NTE2NzA5OTY3Mjg2NTAxMzc2.Dt3oSA.k2NK-9nu2-jB_QoTyeoajIBXRRc'
 
 #Piper test token
 #TOKEN = 'NTE2NzIzMjM0NjY2OTcxMTQ2.Dt30Uw.MqF7ofYtE2GiWTBGSg9hjv_9CIQ'
 
-client = discord.Client()
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
+client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.command(
+    name='vuvuzela',
+    description='Plays an awful vuvuzela in the voice channel',
+    pass_context=True,
+)
+async def vuvuzela(context):
+    user=context.message.author
+    voice_channel=user.voice.voice_channel
+    channel=None
+    if voice_channel!= None:
+        channel=voice_channel.name
+        await client.say('User is in channel: '+ channel)
+        vc= await client.join_voice_channel(voice_channel)
+        player = vc.create_ffmpeg_player('vuvuzela.mp3', after=lambda: print('done'))
+        player.start()
+        while not player.is_done():
+            await asyncio.sleep(1)
+        player.stop()
+        await vc.disconnect()
+    else:
+        await client.say('User is not in a channel.')
+
+
+@client.command(
+    name='play',
+    description='Plays the audio from a specified youtube link.',
+    pass_context=True,
+)
+async def play(context, url):
+    user=context.message.author
+    voice_channel = user.voice.voice_channel
+    channel = None
+    if voice_channel != None:
+        vc = await client.join_voice_channel(voice_channel)
+        player = await vc.create_ytdl_player(url=url)
+        await client.say('User is in channel: '+ voice_channel.name)
+        await client.say('Now playing: '+ player.title)
+        player.start()
+        while not player.is_done():
+            await asyncio.sleep(1)
+        player.stop()
+        await vc.disconnect()
+    else:
+        await client.say('User is not in a channel.')
+
+
+@client.command(
+    name='roll',
+    description='Rolls dice. Format is [Number of dice to be rolled]d[Max value of dice]',
+    pass_context=True,
+)
+async def roll(context, die):
+    num_die=die.split('d')[0]
+    max_val=die.split('d')[1]
+    results=[]
+    total=0
+    if int(num_die)<=0 or int(max_val)<1:
+        await client.say('Stop trying to fuck with my bot Zach')
         return
-
-    if message.content == '!piper':
-        msg = 'Hello, my name is Piper. Type "!piper-help" to see more options.'
-        await client.send_message(message.channel, msg)
-
-    if '!piper' in str(message.content):
-        message_tokens=tokenize(str(message.content))
-        if len(message_tokens)>=2:
-            if message_tokens[1] == 'vuvuzela':
-                user=message.author
-                voice_channel=user.voice.voice_channel
-                channel='None'
-                if voice_channel != None:
-                    channel=voice_channel.name
-                    msg = 'User is in channel: ' + channel
-                    await client.send_message(message.channel, msg)
-                    vc = await client.join_voice_channel(voice_channel)
-                    player=vc.create_ffmpeg_player('vuvuzela.mp3', after=lambda: print('done'))
-                    player.start()
-                    while not player.is_done():
-                        await asyncio.sleep(1)
-                    player.stop()
-                    await vc.disconnect()
-                else:
-                    msg='User is not in a channel.'
-                    await client.send_message(message.channel, msg)
-            elif message_tokens[1] == 'play':
-                user = message.author
-                voice_channel = user.voice.voice_channel
-                channel = 'None'
-                if voice_channel != None:
-                    channel = voice_channel.name
-                    vc = await client.join_voice_channel(voice_channel)
-                    player = await vc.create_ytdl_player(url=message_tokens[2])
-                    msg = 'User is in channel: ' + channel
-                    await client.send_message(message.channel, msg)
-                    msg = 'Now playing: '+player.title
-                    await client.send_message(message.channel, msg)
-                    player.start()
-                    while not player.is_done():
-                        await asyncio.sleep(1)
-                    player.stop()
-                    await vc.disconnect()
-                else:
-                    msg='User is not in a channel.'
-                    await client.send_message(message.channel, msg)
+    for die in range(1, int(num_die)+1):
+        x=random.randrange(1, int(max_val)+1)
+        results.append(x)
+        total+=x
+    msg=''
+    for result in results:
+        msg+=str(result)+' + '
+    if msg.endswith(' + '):
+        msg=msg[:-3]
+    await client.say(msg)
+    await client.say('Total is: '+str(total))
 
 
 @client.event
@@ -69,11 +94,5 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-
-
-def tokenize(commands):
-    message_tokens=commands.split(' ')
-    return message_tokens
-
 
 client.run(TOKEN)
